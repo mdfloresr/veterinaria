1083d30eb3344481d8abcf4db8e1961b1e914b97
mdfloresr452 <mdflores@unjbg.edu.pe>
2024-07-16 23:30:50 -0500

    2da actualizacion del config

--- src/config.js
+++ src/config.js
@@ -3,11 +3,11 @@ require('dotenv').config();
 const config = {
   env: process.env.NODE_ENV || 'dev',
   port: process.env.NODE_PORT || '3000',
-  dbuser: process.env.DB_USER,
-  dbPassword: process.env.DB_PASSWORD,
-  dbHost: process.env.DB_HOST,
-  dbNAME: process.env.DB_NAME,
-  dbPORT: process.env.DB_PORT,
+  dbuser: process.env.DB_USER ||  'root',
+  dbPassword: process.env.DB_PASSWORD || 'tsditlAOnOqikXJvNdtTkpOLkDIMeebT',
+  dbHost: process.env.DB_HOST || 'roundhouse.proxy.rlwy.net',
+  dbNAME: process.env.DB_NAME || 'crm_oficial2',
+  dbPORT: process.env.DB_PORT || '50019',
 }
 
 module.exports = {config};
